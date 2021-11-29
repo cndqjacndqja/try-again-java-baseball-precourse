ad5203d65f047c63b37d94b1833b67b79b08842a
jochungbeom <cndqjacndqja@gmail.com>
2021-11-29 20:37:47 +0900

    feat: 숫자 입력 뷰, 사용자 숫자 입력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.view.InputView;
+
 public class Game {
+	Computer computer = new Computer();
+
+	public Game() {
+		computer.createBalls();
+	}
+
 	public void run() {
+		String input = InputView.inputNumbers();
 
 	}
 }
