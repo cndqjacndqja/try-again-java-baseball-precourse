dfd7e0671b83b7b9a28a42f0ff6e1e369d87ab25
jochungbeom <cndqjacndqja@gmail.com>
2021-11-29 20:59:49 +0900

    feat: 사용자 입력 숫자로 야구공 생성하기

--- src/main/java/baseball/domain/BallRepository.java
+++ src/main/java/baseball/domain/BallRepository.java
@@ -7,8 +7,13 @@ public class BallRepository {
 	List<Ball> ballList = new ArrayList<>();
 
 	public void insertBall(List<Integer> numbers) {
+		clearBallList();
 		for (int i = 0; i < numbers.size(); i++) {
 			ballList.add(new Ball(numbers.get(i)));
 		}
+	}
+
+	public void clearBallList() {
+		ballList.clear();
 	}
 }
---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,40 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class User {
+	BallRepository ballRepository = new BallRepository();
+
+	public void insertNumbers(String input) {
+		ballRepository.insertBall(createBallNumbers(input));
+	}
+
+	private List<Integer> createBallNumbers(String input) {
+		validateDuplicate(input);
+		List<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < input.length(); i++) {
+			int number = Character.getNumericValue(input.charAt(i));
+			validateNumber(number);
+			numbers.add(number);
+		}
+		return numbers;
+	}
+
+	private void validateDuplicate(String input) {
+		LinkedHashSet<String> tempSet = new LinkedHashSet<>(Arrays.asList(input.split("")));
+		if(tempSet.size() != 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateNumber(int number) {
+		if(number >= 1 && number <= 9) {
+			return;
+		}
+		throw new IllegalArgumentException();
+	}
+
+}
