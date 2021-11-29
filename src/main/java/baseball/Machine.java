8e515c84ac0b19dbdee6632d543d84983e8c0615
jochungbeom <cndqjacndqja@gmail.com>
2021-11-29 20:03:04 +0900

    feat: 게임 종료하기

--- src/main/java/baseball/Machine.java
+++ src/main/java/baseball/Machine.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.view.InputView;
+
 public class Machine {
 	public void run() {
+		Game game = new Game();
+		game.run();
+		if(whetherRestart()) {
+			return;
+		}
+		run();
+	}
 
+	public boolean whetherRestart() {
+		return InputView.requestWhetherRestart().equals("2");
 	}
 }
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	public static String requestWhetherRestart() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		return Console.readLine();
+	}
+}
