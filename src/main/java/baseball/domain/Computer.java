45f415a8aa8ee2cb0d05bb60d815ebcd5b44b99b
jochungbeom <cndqjacndqja@gmail.com>
2021-11-29 20:31:29 +0900

    feat: 임의의 수 3개를 랜덤으로 생성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+	BallRepository ballRepository = new BallRepository();
+
+	private List<Integer> createRandomNumbers() {
+		LinkedHashSet<Integer> tempSet = new LinkedHashSet<>();
+		while (tempSet.size() != 3) {
+			tempSet.add(Randoms.pickNumberInRange(1, 9));
+		}
+		return new ArrayList<>(tempSet);
+	}
+}
